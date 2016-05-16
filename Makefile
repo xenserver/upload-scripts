e710c9a2f0ac52f2e922457da6eb74ca702ccddc
Jon Ludlam <jonathan.ludlam@citrix.com>
2016-05-16 17:14:21 +0100

    Add ocaml variant of the script
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: clean default
+
+default: update_xs_yum.native
+
+clean:	
+	rm -rf *.native *~ 449* _build *.iso
+
+update_xs_yum.native: src/update_xs_yum.ml
+	ocamlbuild -use-ocamlfind src/update_xs_yum.native
+
---
+++ src/update_xs_yum.ml
@@ -0,0 +1,231 @@
+
+(*
+ * Copyright (C) 2015 Citrix Systems Inc
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+open Lwt
+open Iso
+open Astring
+
+module Iso = Isofs.Make(Block)(Io_page)
+
+exception Error
+let (>>*=) m f = match m with
+  | `Error e -> fail Error
+  | `Ok x -> f x
+
+let ok = `Ok ()
+
+let (>>|=) m f = m >>= fun x -> x >>*= f
+
+let mkdir_safe dir perm =
+  Lwt.catch
+    (fun () -> Lwt_unix.mkdir dir perm)
+    (function | Unix.Unix_error (Unix.EEXIST, _, _) -> Lwt.return ()
+              | e -> fail e)
+
+(** create a directory, and create parent if doesn't exist *)
+let mkdir_p dir perm =
+  Printf.printf "mkdir_p %s %d\n" dir perm;
+  let rec p_mkdir dir =
+    let p_name = Filename.dirname dir in
+    (if p_name <> "/" && p_name <> "." 
+     then p_mkdir p_name
+     else Lwt.return ()) >>= fun () ->
+    mkdir_safe dir perm in
+ p_mkdir dir >>= fun () -> Lwt.return ok
+
+(* Dump a list of cstructs into a file f *)
+let cstrs_to_file cstrs f =
+  Lwt_unix.openfile f [Lwt_unix.O_RDWR; Lwt_unix.O_CREAT] 0o644
+  >>= fun fd ->
+  Lwt_list.iter_s (
+    fun cstr -> Lwt_cstruct.write fd cstr
+      >>= fun _ -> Lwt.return ())
+    cstrs
+  >>= fun () ->
+  Lwt_unix.close fd
+
+let default_filtermap = function
+  | `D name -> Some name
+  | `F name -> Some name
+
+(* Sync an iso filesystem to a local filesystem. Optionally maps dir names and filenames *)
+let sync ?(filtermap=default_filtermap) iso entries lrelpath irelpath =
+  mkdir_p lrelpath 0o755
+  >>|= fun () -> 
+  let rec inner entries lrelpath irelpath = 
+    Lwt_list.fold_left_s
+      (fun acc f ->
+         match f with
+         | (name, Isofs.Directory d) ->
+           acc >>*= fun () ->
+           (match filtermap (`D name) with
+            | Some name' ->
+              mkdir_p (Filename.concat lrelpath name') 0o755
+              >>|= fun () ->
+              inner
+                d.Isofs.d_contents
+                (Filename.concat lrelpath name')
+                (Filename.concat irelpath name)
+            | None -> 
+              Lwt.return ok)
+         | (name, Isofs.File f) ->
+           acc >>*= fun () ->
+           (match filtermap (`F name) with
+            | Some name' ->
+              let ipath = Filename.concat irelpath name in
+              Iso.KV_RO.size iso ipath
+              >>|= fun size ->
+              Iso.KV_RO.read iso ipath 0 (Int64.to_int size)
+              >>|= fun result ->
+              let filename = Filename.concat lrelpath name' in
+              Printf.printf "%s\n" filename;
+              cstrs_to_file result filename >>= fun () ->
+              Lwt.return ok
+            | None ->
+              Lwt.return ok)
+      ) ok entries
+  in inner entries lrelpath irelpath
+
+let create_tree root binpkg_iso sources_iso =
+  let rpmsdir = Filename.concat root "RPMS" in
+  let srpmsdir = Filename.concat root "SRPMS" in
+  Block.connect binpkg_iso
+  >>|= fun b ->
+  Iso.connect b
+  >>|= fun iso ->
+  (* Sync binpkg.iso to RPMS dir locally, skipping the 'repodata'
+     directory *)
+  sync ~filtermap:(
+    function
+    | `D "repodata" -> None
+    | `D x -> Some x
+    | `F x -> Some x)
+    iso iso.Iso.entries rpmsdir "/"
+  >>|= fun () ->
+  Block.connect sources_iso
+  >>|= fun b ->
+  Iso.connect b
+  >>|= fun iso ->
+  (* Sync sources.iso to SRPMS dir locally, flattening the dir
+     hierarchy and skipping any file that doesn't look like an SRPM *)
+  sync ~filtermap:(
+    function
+    | `D x -> Some "/"
+    | `F x ->
+      if String.is_suffix ".src.rpm" x
+      then Some x
+      else None)
+    iso iso.Iso.entries srpmsdir "/"
+  >>|= fun () ->
+  Lwt.return ok
+
+let check th =
+  th >>= function
+  | Lwt_unix.WEXITED x when x=0 -> Lwt.return ok
+  | _ -> Lwt.return (`Error `Bad)
+
+let get uri filename =
+  let open Cohttp in
+  let open Cohttp_lwt_unix in
+  let so_far = ref 0 in
+  let last_percentage = ref (-1) in
+  Client.call `GET (Uri.of_string uri)
+  >>= fun (res, body) ->
+  let status = Response.status res in
+  let res_sexp = Response.sexp_of_t res in
+  Printf.printf "Response: %s\n%!" (Sexplib.Sexp.to_string res_sexp);
+  let headers = Response.headers res in
+  let len =
+    match Header.get headers "content-length" with
+    | Some len ->
+      Printf.printf "Got content-length: %s\n%!" len;
+      Some (int_of_string len)
+    | None ->
+      Printf.printf "No content-length\n%!";
+      None
+  in
+  match Code.is_success (Code.code_of_status status) with
+  | false ->
+    Printf.fprintf stderr "Got bad return code fetching \"%s\": %s"
+      uri (Code.string_of_status status);
+    exit 1
+  | true ->
+    Lwt_io.with_file ~mode:Lwt_io.output filename
+      (fun chan -> Lwt_stream.iter_s
+          (fun s ->
+             so_far := !so_far + (String.length s);
+             (match len with
+             | Some i ->
+               let percent = (100 * !so_far) / i in
+               if !last_percentage <> percent then begin
+                 if percent mod 10 = 0 then
+                   Printf.printf "%d%%%!" percent
+                 else
+                   Printf.printf ".%!";
+                 last_percentage := percent
+               end
+             | None -> ());
+             Lwt_io.write chan s)
+          (Cohttp_lwt_body.to_stream body)) >>= fun () ->
+    match len with
+    | Some i ->
+      if !so_far != i
+      then begin
+        Printf.fprintf stderr "Receieved size <> content-length: (%d <> %d)\n%!" (!so_far) i;
+        exit 1
+      end;
+      Lwt.return ok
+    | None ->
+      Lwt.return ok
+        
+
+let download_isos branch =
+  let uri_base = Printf.sprintf
+      "http://coltrane.uk.xensource.com/usr/groups/build/carbon/%s/xe-phase-3-latest/xe-phase-3"
+      branch in
+
+  let binpkg_uri = Printf.sprintf "%s/binpkg.iso" uri_base in
+  let binpkg_fname = Filename.temp_file "binpkg" "iso" in
+  let sources_uri = Printf.sprintf "%s/source.iso" uri_base in
+  let sources_fname = Filename.temp_file "source" "iso" in
+
+  Printf.printf "Downloading binpkg.iso\n%!";
+  get binpkg_uri binpkg_fname
+  >>|= fun () ->
+  Printf.printf "Downloading source.iso\n%!";
+  get sources_uri sources_fname
+  >>|= fun () ->
+
+  Lwt.return (`Ok (binpkg_fname, sources_fname))
+  
+                                       
+let run root branch s3bin =
+  download_isos branch
+  >>|= fun (binpkg_iso, sources_iso) ->
+  Printf.printf "Running createtree\n%!";
+  create_tree root binpkg_iso sources_iso
+  >>|= fun () ->
+  Printf.printf "Running createrepo\n%!";
+  check (Lwt_unix.system (Printf.sprintf "createrepo %s" root))
+  >>|= fun _ ->
+  Printf.printf "Running s3cmd sync\n%!";
+  check (Lwt_unix.system (Printf.sprintf "s3cmd sync %s %s" root s3bin))
+  >>|= fun _ -> Lwt.return ok
+
+let _ =
+  Lwt_main.run (run "449e52a4-271a-483a-baa7-24bf362866f7" "trunk-ring3" "s3://xs-yum-repos/")
+    
