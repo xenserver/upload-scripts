14e0f2087518847b2034103cbed8b0f9ce61b635
Frederico Mazzone <frederico.mazzone@citrix.com>
2017-03-16 17:47:09 +0000

    Update generate-whitelist script to include main toolstack packages
    Signed-off-by: Frederico Mazzone <frederico.mazzone@citrix.com>

--- generate-whitelist.sh
+++ generate-whitelist.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# This script is supposed to be run inside a planex-buildenv container
-# in order to generate a whitelist of packages needed to build all the
-# components in which we have Travis CI enabled on GitHub.
+# This script is supposed to be run inside a planex-buildenv container, for a
+# specific xenserver branch, in order to generate a whitelist of packages
+# needed to build all the components in which we have Travis CI enabled on
+# GitHub (for that branch). The whitelist for that branch can then be merged
+# into the existing whitelist in update_xs_yum.ml
 
 set -e
 
-# update this list to support all the packages which we will use Travis CI
-PACKAGES="xapi.src xenopsd.src xcp-networkd.src forkexecd.src xcp-rrdd.src squeezed.src vhd-tool.src xenops-cli.src gpumon.src"
+# update this list to support all the packages for which we will use Travis CI
+SOURCES="xapi.src xenopsd.src xcp-networkd.src forkexecd.src xcp-rrdd.src squeezed.src vhd-tool.src xenops-cli.src gpumon.src" 
+PACKAGES=`echo $SOURCES | sed 's/\.src//g'`
 
-# print out the dependencies, excluding ones from CentOS, as those can be obtained directly from them and shouldn't need uploading
-repoquery --disablerepo=base --disablerepo=epel --disablerepo=extras --disablerepo=updates --requires --recursive --resolve $PACKAGES | sort | sed 's/^\(.*\)\-[^\-]*\-[^\-]*$/\1/' | uniq
+# get the dependencies, excluding upstream ones, as those can be obtained directly and shouldn't need uploading
+BUILD_DEPS=`repoquery --disablerepo=base --disablerepo=epel --disablerepo=extras --disablerepo=updates --requires --recursive --resolve $SOURCES | sed 's/^\(.*\)\-[^\-]*\-[^\-]*$/\1/'`
+
+echo $PACKAGES $BUILD_DEPS | tr " " "\n" | sort | uniq
+
